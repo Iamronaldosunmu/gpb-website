a8825cc70d7096773d0a5ef065dab641ae2b2a8f
Ronald Dosunmu <76405995+Iamronaldosunmu@users.noreply.github.com>
2023-09-05 07:47:18 +0100

    Created the footer section

---
+++ src/sections/Footer/index.tsx
@@ -0,0 +1,13 @@
+import BottomFooter from "./BottomFooter";
+import SubscribeForm from "./SubscribeForm";
+
+const Footer = () => {
+  return (
+    <>
+      <SubscribeForm />
+      <BottomFooter />
+    </>
+  );
+};
+
+export default Footer;
