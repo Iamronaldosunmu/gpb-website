d618bce1bc70ad44078806a2c69900ddd9af74aa
Ronald Dosunmu <76405995+Iamronaldosunmu@users.noreply.github.com>
2023-09-05 07:47:56 +0100

    finished working on the home page

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -1,5 +1,30 @@
+import Nav from "../components/nav";
+import SectionGrid from "../components/sectiongrid";
+import BottomFooter from "../sections/Footer/BottomFooter";
+import SubscribeForm from "../sections/Footer/SubscribeForm";
+import Clients from "../sections/home/Clients";
+import Fabrics from "../sections/home/Fabrics";
+import FeaturedIn from "../sections/home/FeaturedIn";
+import AboutUs from "../sections/home/aboutus";
+import Features from "../sections/home/features";
+import Hero from "../sections/home/hero";
+import ShopCollections from "../sections/home/shopcollections";
+
 const Home: React.FC = () => {
-  return <main></main>;
+  return (
+    <main>
+      <Nav />
+      <Hero />
+      <ShopCollections />
+      <AboutUs />
+      <Features />
+      <Fabrics />
+      <Clients />
+      <FeaturedIn />
+      <SubscribeForm />
+      <BottomFooter />
+    </main>
+  );
 };
 
 export default Home;
