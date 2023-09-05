cb4a57966910abe8febb852a6ef79a11f9989601
Ronald Dosunmu <76405995+Iamronaldosunmu@users.noreply.github.com>
2023-09-05 07:52:11 +0100

    added the home and about us routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import { Routes, Route } from "react-router-dom";
 import Home from "./pages/home";
+import AboutUs from "./pages/about-us";
 
 function App() {
   return (
     <>
       <Routes>
-        <Route path="/" element={<Home />} />
+        <Route path="/home" element={<Home />} />
+        <Route path="/about-us" element={<AboutUs />} />
       </Routes>
     </>
   );
