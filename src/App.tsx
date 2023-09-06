f7fa2af30a28193de29968af15aa1f43402bdaea
Ronald Dosunmu <76405995+Iamronaldosunmu@users.noreply.github.com>
2023-09-06 19:39:01 +0100

    Changed the route name

--- src/App.tsx
+++ src/App.tsx
@@ -3,15 +3,13 @@ import Home from "./pages/home";
 import AboutUs from "./pages/about-us";
 import OrderSummary from "./pages/orderSummary";
 
-
 function App() {
-
   return (
     <>
       <Routes>
         <Route path="/home" element={<Home />} />
         <Route path="/about-us" element={<AboutUs />} />
-        <Route path="/orderSummary" element={<OrderSummary />} />
+        <Route path="/order-summary" element={<OrderSummary />} />
       </Routes>
     </>
   );
