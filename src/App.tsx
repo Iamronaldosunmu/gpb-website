2a113c8589c76f84daf8f92682e8181e36486334
Ronald Dosunmu <76405995+Iamronaldosunmu@users.noreply.github.com>
2023-09-06 19:18:38 +0100

    Added the get a quote and the clients page

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,8 @@
 import { Routes, Route } from "react-router-dom";
 import Home from "./pages/home";
 import AboutUs from "./pages/about-us";
+import GetAQuote from "./pages/get-a-quote";
+import Clients from "./pages/clients";
 
 function App() {
   return (
@@ -8,6 +10,8 @@ function App() {
       <Routes>
         <Route path="/home" element={<Home />} />
         <Route path="/about-us" element={<AboutUs />} />
+        <Route path="/get-a-quote" element={<GetAQuote />} />
+        <Route path="/clients/:name" element={<Clients />} />
       </Routes>
     </>
   );
