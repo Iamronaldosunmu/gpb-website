8d47ad656b2d772bf688ea9b7aee7f11ce01bce3
jason <jason.aghedo@gmail.com>
2023-09-07 00:28:54 +0100

    Changed the Implementation of Product component

--- src/components/product/index.tsx
+++ src/components/product/index.tsx
@@ -1,29 +1,60 @@
-const Product = () => {
+import { useState } from "react";
+import { motion } from "framer-motion";
+import { defaultEase } from "../../utils/framer-default-animations";
+import Like from "../like";
+
+interface ProductProps {
+	oldPrice?: string;
+	newPrice?: string;
+	productName?: string;
+	// productImage?: string;
+}
+
+const Product: React.FC<ProductProps> = () => {
+	const [isHovered, setIsHovered] = useState(false);
+	const [isLiked, setIsLiked] = useState(false);
 	return (
-		<div className="relative w-[282px] border border-[#3C3B3B] rounded-sm">
-			<div className="w-[50px] h-[50px] flex justify-center items-center absolute top-2 right-[6px] bg-white rounded-full">
-				<img
-					className="w-[28px] h-[28px] object-cover"
-					src="/assets/images/favorite.svg"
-					alt=""
-				/>
-			</div>
-			<div className="w-full h-[408px] overflow-hidden">
-				<img
+		<div
+			onMouseEnter={() => setIsHovered(true)}
+			onMouseLeave={() => setIsHovered(false)}
+			className="w-[282px] h-[283px] overflow-hidden relative border border-[#3C3B3B] rounded-sm cursor-pointer"
+		>
+			<div className="overflow-hidden scale-[1.01]">
+				<motion.img
+					animate={{
+						scale: isHovered ? 1.1 : 1,
+						transition: { duration: 0.4, ease: defaultEase },
+					}}
 					className="object-cover w-full h-full"
 					src="/assets/patterns/blue-beauty.png"
-					alt=""
 				/>
 			</div>
-			<div className="px-5 py-3 font-semibold flex items-center text-[15px] justify-between bg-white">
-				<span>SHINE</span>
-				<div>
-					<span className="font-lato">N80,000</span>
-					<span className="font-lato line-through text-[#5A3522] ml-2">N98,000</span>
+			<motion.div
+				initial={{ y: 0 }}
+				animate={{
+					y: isHovered ? 0 : 50,
+					transition: { duration: 0.3, ease: defaultEase },
+				}}
+				className="px-5 h-[90px] flex flex-col justify-center text-center absolute bottom-0 w-full font-semibold text-base bg-white"
+			>
+				<div className="flex items-center justify-between">
+					<p>SHINE</p>
+					<div>
+						<span className="font-lato">N80,000</span>
+						<span className="font-lato line-through text-[#5A3522] ml-2">N98,000</span>
+					</div>
 				</div>
-			</div>
+				<div className="flex justify-between items-center mt-[18px] transition-all duration-500 ease-in-out">
+					<button className="outline-0 bg-black py-1 px-[27px] text-white">Add To Cart</button>
+					<div
+						onClick={() => setIsLiked(!isLiked)}
+						className="px-[11px] outline-0"
+					>
+						<Like liked={isLiked} />
+					</div>
+				</div>
+			</motion.div>
 		</div>
 	);
 };
-
 export default Product;
--- src/pages/shop-print.tsx
+++ src/pages/shop-print.tsx
@@ -4,34 +4,47 @@ import Product from "../components/product";
 import Footer from "../sections/Footer";
 
 function ShopPrint() {
+	const navItems = ["Home", "Shop print"];
 	return (
-		<div className="pt-[169px]">
+		<div className="pt-[180px]">
 			<Nav />
-			<Container>
-				<header className="flex flex-col text-[#181818] pt-7">
-					<h1 className="text-[32px] font-bold">Shop Prints</h1>
-					<span className="text-xl font-medium self-end mt-6">
-						Home / <span className="text-[#828282]">shop print</span>
-					</span>
-					<div className="flex mt-[61px] self-end w-full justify-between items-center">
-						<div className="font-medium text-[23px] space-x-2 relative">
-							<span className="relative bottom-[3px]">53</span>
-							<span className="">Products</span>
-						</div>
-						<div className="flex items-center justify-center py-4 px-3 border border-[#181818] gap-x-1">
-							<span className="text-xl font-medium">Sort by: featured </span>
-							<img
-								className="w-6 h-6 object-cover"
-								src="/assets/images/chevron-down.svg"
-								alt=""
-							/>
-						</div>
+			<header className="relative w-full text-[#181818] h-[321px] bg-[url('/assets/images/heroimage.png')] bg-no-repeat bg-cover bg-center bg-local">
+				<Container className="relative h-full">
+					<div className="absolute bottom-0 text-[32px] font-bold bg-white pt-7 text-center px-[73px]">Shop Prints</div>
+				</Container>
+			</header>
+			<Container className="mt-4">
+				<div className="text-xl text-right">
+					<ul className="list-none p-0 font-semibold">
+						{navItems.map((item, index) => (
+							<li
+								className="cursor-pointer inline-block mr-1"
+								key={index}
+							>
+								<span className="hover:text-gray-400">{item}</span>
+								{index !== navItems.length - 1 ? " / " : ""}
+							</li>
+						))}
+					</ul>
+				</div>
+				<div className="flex mt-[61px] self-end w-full justify-between items-center">
+					<div className="font-medium text-[23px] space-x-2 relative">
+						<span className="relative bottom-[3px]">53</span>
+						<span className="">Products</span>
 					</div>
-				</header>
+					<div className="flex items-center justify-center py-4 px-3 border border-[#181818] gap-x-1">
+						<span className="text-xl font-medium">Sort by: featured </span>
+						<img
+							className="w-6 h-6 object-cover"
+							src="/assets/images/chevron-down.svg"
+							alt=""
+						/>
+					</div>
+				</div>
 			</Container>
 			<Container className="mt-[51px] mb-[59px] flex flex-col items-center justify-center">
-				<section className="grid grid-cols-4 gap-x-[30px] gap-y-[90px] mb-[110px]">
-					{[1, 2, 3, 4, 5, 6, 7, 9, 10, 11, 12, 13, 14, 15, 16, 17].map((i) => (
+				<section className="grid grid-cols-4 gap-x-[30px] gap-y-[116px] mb-[110px]">
+					{[1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12].map((i) => (
 						<Product key={i} />
 					))}
 				</section>
