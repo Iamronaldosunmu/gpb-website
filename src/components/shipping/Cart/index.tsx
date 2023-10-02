2518a4498dc4b9fb2208ac24544348f732bcca42
Ronald Dosunmu <76405995+Iamronaldosunmu@users.noreply.github.com>
2023-10-03 00:28:08 +0100

    added the button to close the modal

--- src/components/shipping/Cart/index.tsx
+++ src/components/shipping/Cart/index.tsx
@@ -1,10 +1,10 @@
 import { faLock } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import useCartStore from "../../../store/cart";
-import useProductStore from "../../../store/products";
+import useProductStore, { Product } from "../../../store/products";
+import { Link } from "react-router-dom";
 
-
-const Cart = ({ closeModal?: (arg: boolean) => void }: { closeModal?: (arg: boolean) => void }) => {
+const Cart = ({ closeModal }: { closeModal?: (arg: boolean) => void }) => {
 	const { cart } = useCartStore();
 	const { products } = useProductStore();
 	const getSubTotal = () => {
@@ -22,21 +22,29 @@ const Cart = ({ closeModal?: (arg: boolean) => void }: { closeModal?: (arg: bool
 
 	const totalCost = subTotal + totalShippingFee + tax;
 
-  return (
-    <div className="flex h-full ">
-      <div className="bg-red-100 flex-grow p-6 px-9 max-w-md  mt-4">
-        <div className="flex justify-between mb-1 mt-6  ">
-          <h2 className="text-2xl font-semibold">Order Summary</h2>
-		  <Link
+	return (
+		<div className="flex h-full ">
+			<div className="bg-red-100 flex-grow p-6 px-9 max-w-md  mt-4">
+				<div className="flex justify-end pt-8 mt-10 md:mt-0">
+					<button
+						className="lg:hidden"
+						onClick={() => closeModal?.(false)}
+					>
+						X
+					</button>
+				</div>
+				<div className="flex justify-between mb-1 mt-6  ">
+					<h2 className="text-2xl font-semibold">Order Summary</h2>
+					<Link
 						className="underline mx-4"
 						to="/cart"
 					>
 						Edit cart
 					</Link>
-        </div>
-        <div className="border-b border-b-black mb-12"></div>
-        <div>
-          {cart.map((item, index) => (
+				</div>
+				<div className="border-b border-b-black mb-12"></div>
+				<div>
+					{cart.map((item, index) => (
 						<div
 							key={index}
 							className="grid grid-cols-3 pb-9 "
@@ -47,64 +55,62 @@ const Cart = ({ closeModal?: (arg: boolean) => void }: { closeModal?: (arg: bool
 									alt={products?.find((product: Product) => product.id === item.id)?.name || ""}
 									className="w-20 h-20 object-cover flex-shrink-0 sm:mr-5 mr-2"
 								/>
-                <div className="flex-shrink-0">
-								<h3 className="text-base font-semibold">{item.name}</h3>
-								<p className="text-xs">COLOR : {item.color}</p>
-								<p className="text-xs">EXCLUSIVITY : {item.exclusivity ? "YES" : "NO"}</p>
-							</div>
+								<div className="flex-shrink-0">
+									<h3 className="text-base font-semibold">{products?.find((product: Product) => product.id === item.id)?.name || ""}</h3>
+									<p className="text-xs">COLOR : {item.backgroundColor}</p>
+									<p className="text-xs">EXCLUSIVITY : {item.exclusivity ? "YES" : "NO"}</p>
+								</div>
 							</div>
 
 							<div className="flex flex-shrink justify-end">
-								<p className="text-sm font-semibold ">N{item.price}</p>
+							<p className="text-sm font-semibold price">₦{parseInt(products?.find((product: Product) => product.id === item.id)?.discountPrice || (products?.find((product: Product) => product.id === item.id)?.price as string)).toLocaleString()}</p>
 							</div>
 						</div>
 					))}
-        </div>
-        <div className="mb-7 flex items-center">
-          <input
-            id="apply"
-            className="flex-grow py-3 px-3 mr-1 focus:border-none text-xs h-12 outline-none"
-            placeholder="Enter gift code or voucher"
-            type="text"
-          />
-          <button className="bg-black text-white px-5 py-3 text-center hover:cursor-pointer h-12">
-            Apply
-          </button>
-        </div>
+				</div>
+				<div className="mb-7 flex items-center">
+					<input
+						id="apply"
+						className="flex-grow py-3 px-3 mr-1 focus:border-none text-xs h-12 outline-none"
+						placeholder="Enter gift code or voucher"
+						type="text"
+					/>
+					<button className="bg-black text-white px-5 py-3 text-center hover:cursor-pointer h-12">Apply</button>
+				</div>
 
-        <div className="border-b mt-9 mb-7 border-b-black"></div>
-        <div className="flex justify-between text-lg mb-7 font-semibold">
-          <div>
-            <p>SubTotal</p>
-            <p>Shipping</p>
-            <p>Tax</p>
-          </div>
-          <div>
-            <p>₦{subTotal.toLocaleStri₦g()}</p>
-            <p>{totalShippingFee > 0 ? `N${totalShippingFee}` : "Free"}</p>
-            <p>₦{tax.toLocaleString()}</p>
-          </div>
-        </div>
-        <div className="border-b mb-7 border-b-black"></div>
-        <div className="flex justify-between text-lg mb-[3rem]  font-semibold">
-          <div>
-            <p className="text-2xl font-bold">Total</p>
-          </div>
-          <div>
-            <p>N{totalCost.toLocaleString()}</p>
-          </div>
-        </div>
-        <div className="flex justify-center">
-          <p className="font-bold text-lg">
-            <span className="mr-1">
-              <FontAwesomeIcon icon={faLock} />
-            </span>
-            Secure Checkout
-          </p>
-        </div>
-      </div>
-    </div>
-  );
+				<div className="border-b mt-9 mb-7 border-b-black"></div>
+				<div className="flex justify-between text-lg mb-7 font-semibold">
+					<div>
+						<p>SubTotal</p>
+						<p>Shipping</p>
+						<p>Tax</p>
+					</div>
+					<div>
+						<p>₦{subTotal.toLocaleString()}</p>
+						<p>{totalShippingFee > 0 ? `N${totalShippingFee}` : "Free"}</p>
+						<p>₦{tax.toLocaleString()}</p>
+					</div>
+				</div>
+				<div className="border-b mb-7 border-b-black"></div>
+				<div className="flex justify-between text-lg mb-[3rem]  font-semibold">
+					<div>
+						<p className="text-2xl font-bold">Total</p>
+					</div>
+					<div>
+						<p>N{totalCost.toLocaleString()}</p>
+					</div>
+				</div>
+				<div className="flex justify-center">
+					<p className="font-bold text-lg">
+						<span className="mr-1">
+							<FontAwesomeIcon icon={faLock} />
+						</span>
+						Secure Checkout
+					</p>
+				</div>
+			</div>
+		</div>
+	);
 };
 
 export default Cart;
