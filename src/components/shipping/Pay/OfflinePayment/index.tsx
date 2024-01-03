3776a6a60691cdd7bd575c9eef751f1db00f746c
Ronald Dosunmu <76405995+Iamronaldosunmu@users.noreply.github.com>
2024-01-03 09:31:21 +0100

    Update index.tsx

--- src/components/shipping/Pay/OfflinePayment/index.tsx
+++ src/components/shipping/Pay/OfflinePayment/index.tsx
@@ -1,33 +1,27 @@
-// import BillingAddress from "../BillingAddress";
-// import SaveInfo from "../SaveInfo";
-
 const OfflinePayment = () => {
   return (
     <>
       <div className="pt-7 p-4 w-full ">
-        <div className="rounded-lg border mb-8 bg-gray-200  w-full md:px-9 sm:px-4 px-2 pt-9 pb-5">
-          <div>
+        <div className="rounded-lg border mb-8 bg-gray-200  w-full md:px-9 sm:px-4 px-2 pt-9 pb-5 flex flex-col">
+          <div className="md:text-[18px]">
             <p>Hello! Having trouble paying online?</p>
           </div>
-          <div>
-            <p>Please pay into </p>
+          <div className="mt-[10px]">
+            <p>Please pay into: </p>
           </div>
-          <div>
-            <p> Access(diamond) Bank Grapehall ltd 0050698843 (naira payments only) </p>
+          <div className="mt-[10px]">
+            <p> Access(diamond) Bank <br /> Grapehall ltd 0050698843 <br /> (naira payments only) </p>
           </div>
-          <div>
-            <p>Access (diamond) bank Grapehall ltd 1480377181 (dollar payments only) </p>
+          <div className="mt-[10px]">
+            <p>Access (diamond) Bank <br /> Grapehall ltd 1480377181 <br /> (dollar payments only) </p>
           </div>
-          <div>
-            <p>
-              Please send payment receipt to
-              <a href="mailto:info@grapespatternbank.com">info@grapespatternbank.com</a>
+          <div className="mt-[12px]">
+            <p className="flex flex-wrap">
+              Please send payment receipt to <a className="underline text-[blue]" href="mailto:info@grapespatternbank.com">info@grapespatternbank.com</a>
             </p>
           </div>
-          <p>Thank you for shopping with us!</p>
+          <p className="mt-[20px] text-[18px]">Thank you for shopping with us!</p>
         </div>
-        {/* <SaveInfo />
-        <BillingAddress /> */}
       </div>
       
       
