26325de799a6602a43adb5ef137576620152a89c
Ronald Dosunmu <76405995+Iamronaldosunmu@users.noreply.github.com>
2024-01-04 22:14:54 +0100

    do hero section for mobile

--- src/sections/home/hero/index.tsx
+++ src/sections/home/hero/index.tsx
@@ -56,8 +56,39 @@ const Hero = () => {
 				className="embla overflow-hidden max-w-[1260px] md:max-w-[1220px] mx-auto w-full rounded-[15px] relative mt-[90px] lg:mt-[169px]"
 				ref={emblaRef}
 			>
-				<div className="embla__container flex rounded-[8px] h-[350px] md:h-[400px] lg:h-[450px] xl:h-[500px]">
+				<div className="embla__container  rounded-[8px] hidden lg:flex h-[350px] md:h-[400px] lg:h-[450px] xl:h-[500px]">
 					{homePageImages?.banner?.desktop.map((image) => (
+						<div className="embla__slide flex-[0_0_100%] px-[10px] rounded-[10px] overflow-hidden relative">
+							<img
+								src={image.url}
+								className="w-full h-[350px] md:h-[400px] lg:h-[450px] xl:h-[500px] object-cover hidden lg:block rounded-[10px]"
+							/>
+							<div className="absolute top-0 left-[10px] right-[10px] bottom-0 bg-gradient-to-b from-[rgba(0,0,0,0)] from-[60.21%] to-[rgba(0,0,0,0.78)] z-10 rounded-[15px]"></div>
+							<div className="absolute bottom-[28px] z-10 left-[10px] right-[10px]">
+								<Container className="z-10">
+									<motion.button
+										{...interactionAnimations}
+										whileTap={{ scale: 0.98 }}
+										className="bg-white p-[10px] px-[20px] md:px-[30px] text-[14px] md:text-[16px] flex items-center space-x-[15px] rounded-full"
+									>
+										<span
+											onClick={() => navigate("/shop")}
+											className="font-semibold"
+										>
+											SHOP NOW
+										</span>
+										<img
+											className="w-[14px] md:w-[16px]"
+											src="/assets/images/arrow.svg"
+										/>
+									</motion.button>
+								</Container>
+							</div>
+						</div>
+					))}
+				</div>
+				<div className="embla__container flex rounded-[8px] h-[350px] md:h-[400px] lg:h-[450px] xl:h-[500px] lg:hidden">
+					{homePageImages?.banner?.mobile.map((image) => (
 						<div className="embla__slide flex-[0_0_100%] px-[10px] rounded-[10px] overflow-hidden relative">
 							<img
 								src={image.url}
